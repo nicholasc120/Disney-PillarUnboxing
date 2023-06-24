80e48c899b44901ede33759b05879e5ac7f27b1c
nicholasc120 <cervania2000@yahoo.ca>
2023-06-23 23:35:45 -0600

    initial commit

---
+++ scripts/content.js
@@ -0,0 +1,39 @@
+function isWindowAspectRatio4to3() {
+    const width = window.outerWidth;
+    const height = window.outerHeight;
+    const aspectRatio = width / height;
+
+    console.log(Math.abs(aspectRatio - (4 / 3)))
+
+    return Math.abs(aspectRatio - (4 / 3)) < 0.1;
+}
+
+if (isWindowAspectRatio4to3()) {
+    // Window is in a 4:3 aspect ratio
+    // Create a new MutationObserver instance
+    const observer = new MutationObserver((mutations) => {
+        // Check each mutation that occurred
+        mutations.forEach((mutation) => {
+            // Check if new nodes were added to the DOM
+            if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                const video = document.querySelector('video');
+                if (video) {
+                    if (video.videoWidth != 640 && video.videoWidth != 0) {
+                        // Check if the added node is a video element
+                        const videoPlayer = document.getElementsByClassName('btm-media-player');
+                        for (let i = 0; i < videoPlayer.length; i++) {
+                            videoPlayer[i].style.transform = 'scale(1.25)'
+                        }
+                        console.log("scaling video")
+                    }
+                }
+            }
+        });
+    });
+
+    // Start observing the document body and its subtree for changes
+    observer.observe(document.body, { childList: true, subtree: true });
+} else {
+    // Window is not in a 4:3 aspect ratio
+    console.log("Window is not in a 4:3 aspect ratio");
+}
